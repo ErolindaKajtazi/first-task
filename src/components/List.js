2b5418b438276b8353f1691e2250967c00c6cf31
EROLINDA KAJTAZI <erolindakajtazi@gmail.com>
2022-08-04 00:17:45 +0200

    .......

---
+++ src/components/List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './List.css';
+import items from './Items';
+
+
+function List() {
+    return (
+      <div className="items">
+        {items.map((item) => (
+          <div className="item">
+            <div className='element'>{item.icon}</div>
+            <div className='desc'>
+              <h3>{item.name}</h3>
+              <p>{item.question}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default List;
